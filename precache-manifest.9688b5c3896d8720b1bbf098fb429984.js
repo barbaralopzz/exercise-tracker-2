d7c7cd1cf37d1f0e9235ae9e9d20e97916e42b32
Barbara Lopez <barbaralopez@Barbaras-MacBook-Air.local>
2025-02-25 19:04:36 -0500

    Updates

---
+++ precache-manifest.9688b5c3896d8720b1bbf098fb429984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a97f7d1ad468faf9a2792488df4f3f4",
+    "url": "/barbaralopzz/exercise-tracker-2.git/index.html"
+  },
+  {
+    "revision": "0ae04d024d60534ef405",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0174b2ec2f183aadab7",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/js/2.19901ea3.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/js/2.19901ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff7e7c651745f4ce7bc",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/js/3.a6519409.chunk.js"
+  },
+  {
+    "revision": "0ae04d024d60534ef405",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/js/main.ddc0e6f1.chunk.js"
+  },
+  {
+    "revision": "de5f18e18eb34c04ac11",
+    "url": "/barbaralopzz/exercise-tracker-2.git/static/js/runtime-main.7d26c322.js"
+  }
+]);
